89cb2a008f70a28721974ce4f31f2ef44289dfb3
Meskis4 <147125301+Meskis4@users.noreply.github.com>
2025-09-20 23:15:17 +0300

    Išskirčių tvarkymas
    Pakeistas kodas, kad pašalintų tik ekstremalias išskirtis, o sąlyginės lieka. Prieš tai buvo ištrinamos visos išskirtys, nors skaidrėj parašyta, kad reikia atmesti ekstremalias. Ir dar kur failo path pridėjau po vieną taškelį, nes man kitaip kodas neveikė :D

--- scripts/outliers_and_normalization.py
+++ scripts/outliers_and_normalization.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('./data/clean_data.csv')
+df = pd.read_csv('../data/clean_data.csv')
 
 selected_columns = ['Units_Sold', 'Sales', 'Discounts', 'Sale_Price', 'Gross_Sales']
 df_selected = df[selected_columns].copy()
+numeric_cols = selected_columns
 
-numeric_cols = ['Units_Sold', 'Sales', 'Discounts', 'Sale_Price', 'Gross_Sales']
 stats = df_selected[numeric_cols].agg(['min', 'max', 'mean', 'median', 'var'])
 stats.loc['1 kvartilė'] = df_selected[numeric_cols].quantile(0.25)
 stats.loc['3 kvartilė'] = df_selected[numeric_cols].quantile(0.75)
@@ -29,7 +29,7 @@ def format_number(x):
 for col in numeric_cols:
     stats[col] = stats[col].apply(format_number)
 
-print("\nAprašomosios statistikos lentelė prieš atsiskyrėlius:")
+print("\nAprašomosios statistikos lentelė prieš atmetant išskirtis:")
 print(stats)
 
 def find_outliers(df, columns):
@@ -38,23 +38,26 @@ def find_outliers(df, columns):
         Q1 = df[col].quantile(0.25)
         Q3 = df[col].quantile(0.75)
         IQR = Q3 - Q1
-        lower_bound = Q1 - 1.5 * IQR
-        upper_bound = Q3 + 1.5 * IQR
-        outliers[col] = df[(df[col] < lower_bound) | (df[col] > upper_bound)][col]
+
+        lower_outer = Q1 - 3 * IQR
+        upper_outer = Q3 + 3 * IQR
+
+        extreme = df[(df[col] < lower_outer) | (df[col] > upper_outer)][col]
+
+        outliers[col] = extreme.tolist()
     return outliers
 
 outliers = find_outliers(df_selected, numeric_cols)
 
-print("\nTaškai atsiskyrėliai:")
+print("\nIšimtys:")
 for col, vals in outliers.items():
-    print(f"{col}: {vals.tolist()}")
-
+    print(f"{col}: {vals}")
 
 df_no_outliers = df_selected.copy()
 for col, vals in outliers.items():
     df_no_outliers = df_no_outliers[~df_no_outliers[col].isin(vals)]
 
-df_no_outliers.to_csv('./data/clean_data_without_outliers.csv', index=False)
+df_no_outliers.to_csv('../data/clean_data_without_outliers.csv', index=False)
 
 stats_no_outliers = df_no_outliers[numeric_cols].agg(['min', 'max', 'mean', 'median', 'var'])
 stats_no_outliers.loc['1 kvartilė'] = df_no_outliers[numeric_cols].quantile(0.25)
@@ -70,33 +73,30 @@ stats_no_outliers = stats_no_outliers.rename(index={
 for col in numeric_cols:
     stats_no_outliers[col] = stats_no_outliers[col].apply(format_number)
 
-print("\nAprašomosios statistikos lentelė po atsiskyrėlių pašalinimo:")
+print("\nAprašomosios statistikos lentelė po išimčių pašalinimo:")
 print(stats_no_outliers)
 
-df_cleaned = pd.read_csv('./data/clean_data_without_outliers.csv')
 
+means = df_no_outliers.mean()
+stds = df_no_outliers.std(ddof=1)
+df_standardized = (df_no_outliers - means) / stds
 
-means = df_selected.mean()  # vidurkiai
-variances = df_selected.var(ddof=1)  # dispersijos (su m-1 dalikliu)
-stds = np.sqrt(variances)  # standartiniai nuokrypiai
-
-df_standardized = (df_selected - means) / stds
-
-print("\nSunormuoti duomenys (pagal vidurkį ir dispersiją, Vidurkis ir dispersija):")
+print("\nSunormuoti duomenys (pagal vidurkį ir dispersiją):")
 print(df_standardized.head())
 
 
-mins = df_selected.min()
-maxs = df_selected.max()
+mins = df_no_outliers.min()
+maxs = df_no_outliers.max()
+df_minmax = (df_no_outliers - mins) / (maxs - mins)
 
-df_minmax = (df_selected - mins) / (maxs - mins)
-
-print("\nSunormuoti duomenys (pagal Min-Max [0,1]):")
+print("\nSunormuoti duomenys (Min-Max [0,1]):")
 print(df_minmax.head())
 
-df_standardized.to_csv("./data/normalized_mean_var.csv", index=False)
-df_minmax.to_csv("./data/normalized_minmax.csv", index=False)
 
-print("Sunormuota visa duomenų aibė. Rezultatai įrašyti į failus:")
+df_standardized.to_csv("../data/normalized_mean_var.csv", index=False)
+df_minmax.to_csv("../data/normalized_minmax.csv", index=False)
+
+print("\nSunormuota visa duomenų aibė. Rezultatai įrašyti į failus:")
 print(" - normalized_mean_var.csv (Vidurkis ir dispersija)")
 print(" - normalized_minmax.csv (Min-Max)")
+print(" - clean_data_without_outliers.csv (be ekstremalių atsiskyrėlių)")
