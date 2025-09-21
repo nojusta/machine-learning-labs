9330725971e8d2ec9e1350ce2a80a1838659cfc6
Nojus Stankevičius <Nojus869@gmail.com>
2025-09-21 12:21:14 +0300

    cleanup

--- scripts/visual_analysis.py
+++ scripts/visual_analysis.py
@@ -2,12 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# Įkeliame sunormuotus duomenis
-df_standardized = pd.read_csv("../data/normalized_minmax.csv")   #<-- Pakeitus failus tarp minmax ir mean_var
-                                                                #gaunam lenteles po normalizavicjos ir outlier panaikinimo
+df_standardized = pd.read_csv("../data/normalized_minmax.csv")   
+                                                                
 numeric_cols = df_standardized.columns
 
-# Histogramos kiekvienam požymiui
 plt.figure(figsize=(12, 8))
 for i, col in enumerate(numeric_cols):
     plt.subplot(2, 3, i+1)
@@ -18,19 +16,16 @@ for i, col in enumerate(numeric_cols):
 plt.tight_layout()
 plt.show()
 
-# Stačiakampės diagramos (Boxplot) kiekvienam požymiui
 plt.figure(figsize=(10, 6))
 sns.boxplot(data=df_standardized)
 plt.title('Boxplot of standardized features')
 plt.xticks(rotation=45)
 plt.show()
 
-# Taškiniai grafikai (scatter plot matrix)
 sns.pairplot(df_standardized)
 plt.suptitle("Scatter plot matrix of standardized features", y=1.02)
 plt.show()
 
-# Šilumos žemėlapis koreliacijai (reikalinga tolimesnei analizei)
 plt.figure(figsize=(8,6))
 sns.heatmap(df_standardized.corr(), annot=True, cmap='coolwarm', fmt=".2f")
 plt.title("Correlation heatmap of standardized features")
